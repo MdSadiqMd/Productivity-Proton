4d6327d26cc81e7594074d2acdc0794ad75733f6
Md.Sadiq <mohammadsadiq4950@gmail.com>
2023-10-26 21:13:24 +0530

    refactor added the tofdoitems

--- productivity-proton/src/Components/Todo.jsx
+++ productivity-proton/src/Components/Todo.jsx
@@ -3,29 +3,32 @@ import './Todo.css'
 import TodoItems from './TodoItems';
 
 const Todo = () => {
-    const [todos,setTodos]=useState(null);
-    const inputRef=useRef(null);
-    const add=()=>{
-        setTodos([...todos,{no:count++,text:inputRef.current.value,display:""}])
-        inputRef.current.value="";
+    const [todos, setTodos] = useState([]);
+    const inputRef = useRef(null);
+    const [count, setCount] = useState(1); // Initialize count
+    const add = () => {
+        setTodos([...todos, { no: count, text: inputRef.current.value, display: "" }]);
+        inputRef.current.value = "";
+        setCount(count + 1);
     }
-    useEffect(()=>{
+    useEffect(() => {
+        // Add any additional logic you need here when todos change
+    }, [todos]);
 
-    },[todos]);
-  return (
-    <div className="todo">
-        <div className="todo-header">To-Do List</div>
-        <div className="add-todo">
-            <input ref={inputRef} type="text" placeholder='Add Todo' />
-            <button>Add</button>
+    return (
+        <div className="todo">
+            <div className="todo-header">To-Do List</div>
+            <div className="add-todo">
+                <input ref={inputRef} type="text" placeholder='Add Todo' />
+                <button onClick={add}>Add</button> {/* Fix: Add parentheses to invoke 'add' */}
+            </div>
+            <div className="todo-list">
+                {todos.map((item, index) => {
+                    return <TodoItems key={index} no={item.no} display={item.display} text={item.text} />
+                })}
+            </div>
         </div>
-        <div onClick={()=>{add}} className="todo-list">
-            {todos.map((item,index)=>{
-                return <TodoItems key={index} no={item.no} display={item.display} text={item.text} />
-            })}
-        </div>
-    </div>
-  )
+    )
 }
 
-export default Todo
+export default Todo;
--- productivity-proton/src/Components/TodoItems.jsx
+++ productivity-proton/src/Components/TodoItems.jsx
@@ -1,19 +1,19 @@
-import React from 'react'
-import tick from './Assets/accept.png'
-import not_tick from './Assets/not_tick'
-import cross from './Assets/bin.png'
+import React from 'react';
+import tick from './Assets/accept.png';
+import not_tick from './Assets/not_tick.png';
+import cross from './Assets/bin.png';
 
-const TodoItems = (no,display,text,) => {
+const TodoItems = ({ no, display, text }) => {
   return (
     <div className="todoitems">
-        <div className="todoitems-container">
-            <img src={not_tick} alt="" />
-            <img src={tick} alt="" />
-            <div className="todoItems-text">{text}</div>
-        </div>
-        <img src={cross} alt="" />
+      <div className="todoitems-container">
+        <img src={not_tick} alt="" />
+        <img src={tick} alt="" />
+        <div className="todoItems-text">{text}</div>
+      </div>
+      <img src={cross} alt="" />
     </div>
-  )
+  );
 }
 
-export default TodoItems
+export default TodoItems;
