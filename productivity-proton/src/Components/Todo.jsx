57588af99740decf16676632ca8332fc1937bded
Md.Sadiq <mohammadsadiq4950@gmail.com>
2023-10-26 21:01:08 +0530

    chore added assests

--- productivity-proton/src/Components/Todo.jsx
+++ productivity-proton/src/Components/Todo.jsx
@@ -1,18 +1,25 @@
-import React, { useRef, useState } from 'react'
+import React, { useEffect, useRef, useState } from 'react'
 import './Todo.css'
 
 const Todo = () => {
     const [todos,setTodos]=useState(null);
     const inputRef=useRef(null);
+    const add=()=>{
+        setTodos([...todos,{no:count++,text:inputRef.current.value,display:""}])
+        inputRef.current.value="";
+    }
+    useEffect(()=>{
+
+    },[todos]);
   return (
     <div className="todo">
         <div className="todo-header">To-Do List</div>
         <div className="add-todo">
-            <input type="text" placeholder='Add Todo' />
+            <input ref={inputRef} type="text" placeholder='Add Todo' />
             <button>Add</button>
         </div>
-        <div className="todo-list">
-
+        <div onClick={()=>{add}} className="todo-list">
+            
         </div>
     </div>
   )
