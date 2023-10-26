90fe9c13b17299d7f2dfb4d70a92bade61692139
Md.Sadiq <mohammadsadiq4950@gmail.com>
2023-10-26 20:48:40 +0530

    refactor added the hooks to manipulate

--- productivity-proton/src/Components/Todo.jsx
+++ productivity-proton/src/Components/Todo.jsx
@@ -1,7 +1,9 @@
-import React from 'react'
+import React, { useRef, useState } from 'react'
 import './Todo.css'
 
 const Todo = () => {
+    const [todos,setTodos]=useState(null);
+    const inputRef=useRef(null);
   return (
     <div className="todo">
         <div className="todo-header">To-Do List</div>
