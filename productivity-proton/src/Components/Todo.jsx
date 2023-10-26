4647e8c9415eaf4e265b3fde7285b24b85220d52
Md.Sadiq <mohammadsadiq4950@gmail.com>
2023-10-26 21:26:42 +0530

    refactor added todo items

--- productivity-proton/src/Components/Todo.jsx
+++ productivity-proton/src/Components/Todo.jsx
@@ -1,18 +1,35 @@
-import React, { useEffect, useRef, useState } from 'react'
-import './Todo.css'
+import React, { useEffect, useRef, useState } from 'react';
+import './Todo.css';
 import TodoItems from './TodoItems';
 
 const Todo = () => {
     const [todos, setTodos] = useState([]);
     const inputRef = useRef(null);
-    const [count, setCount] = useState(1); // Initialize count
+
+    // Retrieve the count from local storage
+    const storedCount = localStorage.getItem("todos-count");
+    const [count, setCount] = useState(storedCount ? parseInt(storedCount) : 1);
+
     const add = () => {
         setTodos([...todos, { no: count, text: inputRef.current.value, display: "" }]);
         inputRef.current.value = "";
         setCount(count + 1);
+
+        // Update the count in local storage
+        localStorage.setItem("todos-count", count + 1);
     }
+
     useEffect(() => {
-        // Add any additional logic you need here when todos change
+        // Retrieve todos from local storage when the component mounts
+        const storedTodos = JSON.parse(localStorage.getItem("todos"));
+        if (storedTodos) {
+            setTodos(storedTodos);
+        }
+    }, []);
+
+    useEffect(() => {
+        // Store todos in local storage whenever todos change
+        localStorage.setItem("todos", JSON.stringify(todos));
     }, [todos]);
 
     return (
@@ -20,11 +37,11 @@ const Todo = () => {
             <div className="todo-header">To-Do List</div>
             <div className="add-todo">
                 <input ref={inputRef} type="text" placeholder='Add Todo' />
-                <button onClick={add}>Add</button> {/* Fix: Add parentheses to invoke 'add' */}
+                <button onClick={add}>Add</button>
             </div>
             <div className="todo-list">
                 {todos.map((item, index) => {
-                    return <TodoItems key={index} no={item.no} display={item.display} text={item.text} />
+                    return <TodoItems key={index} setTodos={setTodos} no={item.no} display={item.display} text={item.text} />
                 })}
             </div>
         </div>
--- productivity-proton/src/Components/TodoItems.jsx
+++ productivity-proton/src/Components/TodoItems.jsx
@@ -3,12 +3,27 @@ import tick from './Assets/accept.png';
 import not_tick from './Assets/not_tick.png';
 import cross from './Assets/bin.png';
 
-const TodoItems = ({ no, display, text }) => {
+const TodoItems = ({ no, display, text, setTodos }) => {
+  const toggle = () => {
+    let data = JSON.parse(localStorage.getItem("todos"));
+    for (let i = 0; i < data.length; i++) {
+      if (data[i].no === no) {
+        if (data[i].display === "") {
+          data[i].display = "line-through";
+        } else {
+          data[i].display = "";
+        }
+        break;
+      }
+    }
+    setTodos(data);
+  };
+
   return (
     <div className="todoitems">
-      <div className="todoitems-container">
-        <img src={not_tick} alt="" />
-        <img src={tick} alt="" />
+      <div className="todoitems-container" onClick={toggle}>
+        {display === "" ? <img src={not_tick} alt="" /> : <img src={tick} alt="" />}
+
         <div className="todoItems-text">{text}</div>
       </div>
       <img src={cross} alt="" />
